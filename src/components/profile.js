11821d61d845f4f537175631515db2a1e1f7952b
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-01-26 12:48:07 -0800

    Added Icons and link to left side

--- src/components/profile.js
+++ src/components/profile.js
@@ -10,8 +10,8 @@ function Profile() {
 					<h2 className="name">Tenyson</h2>
 					<div className="subtext">Junior Software, Mobile & Web developer. <br /> Cyber security enthusiast</div>
 					<div className="social-links">
-					<a href='https://twitter.com/Tenyson05_' target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
-					<a href="https://twitter.com/Tenyson05_" target="_blank" rel="noopener noreferrer"><i className="fa-github-square"></i></a>
+					<a href='https://twitter.com/Tenyson05_' target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
+					<a href="https://twitter.com/Tenyson05_" target="_blank" rel="noopener noreferrer"><i className="fa fa-gitlab"></i></a>
 					<a href="https://twitter.com/Tenyson05_" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a>
 					<a href="https://twitter.com/Tenyson05_" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
 					</div>
