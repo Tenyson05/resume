bba0b2bd4434701223c0419f192b11b71e5fcd80
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-01-28 19:41:43 -0800

    Added Media query

--- src/components/details.js
+++ src/components/details.js
@@ -9,7 +9,7 @@ function Details() {
 				<p>I am a fouth year university student studying computer science at the University of Technology, Jamaica and self teaching cyber security. I enjoy being a bridge between computer science and cyber security because not only do I get to develop apps, software and websites, I also get to develop cyber security tools to keep the cyber world safe.</p>
 			</div>
 			<div className="skills-card">
-				<h3>skills</h3>
+				<h3>Skills</h3>
 				<hr />
 				<div className="skill-category">
 					<strong>LANGUAGES: </strong>
@@ -28,12 +28,14 @@ function Details() {
 				<h3>Experience</h3>
 				<hr />
 				<div className="exp-left">
-					<p>Digicel Jamaica</p>
-					<p id="year">Summer of 2019</p>
+					<p>Digicel Jamaica, </p>
+					<p>Monitored the company’s network for any potential threat that could potentially harm the organization. Researching daily vulnerabilities discovered to keep aware of developing threats.</p>
+					<p className="year">Summer of 2019</p>
 				</div>
 				<div className="exp-right">
-					<p>Hood Daniel Well Co</p>
-					<p id="year">Summer of 2012-2018</p>
+					<p>Hood Daniel Well Co,</p>
+					<p>Sell products, stock taking and offered services to customers.</p>
+					<p className="year">Summer of 2012-2018</p>
 				</div>
 			</div>
 			<div className="education-card">
@@ -41,11 +43,15 @@ function Details() {
 				<hr />
 				<div className="edu-left">
 					<p>University of Technology, Jamaica <br/> Major: Computer science</p>
-					<p id="year">Expected Graduation year: 2020</p>
+					<p className="year">Expected Graduation year: 2020</p>
+				</div>
+				<div className="edu-middle">
+					<p>Belair High School <br /> 4 Cape</p> 
+					<p className="year">Graduated: 2016</p>
 				</div>
 				<div className="edu-right">
-					<p>Belair High School</p>
-					<p id="year">Graduated: 2016</p>
+					<p>Bellefield High School <br /> 8 CXC</p>
+					<p className="year">Graduated 2014</p>
 				</div>
 			</div>
 		</div>
